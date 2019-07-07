a4a5469eb5ea82ed2d8bd4a52d0faf3ca5832567
emerson4440 <40591474+emerson4440@users.noreply.github.com>
2019-07-07 18:34:51 +0200

    Add files via upload

---
+++ Chargebar.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Level_5
+{
+    public class Chargebar : MonoBehaviour
+    {
+        [SerializeField] private GameObject m_chargebar;
+
+        [SerializeField] private GameObject m_trigger;
+
+        [SerializeField] private EndObjectivePhaseTwo m_EndPhase;
+
+        public bool m_Charged;
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+            if (m_chargebar.transform.localScale.x <= 1 && m_chargebar.transform.localScale.x >= 0.025)
+            {
+                m_chargebar.transform.localScale -= new Vector3(0.005f, 0, 0);
+            }
+            if (m_trigger.GetComponent<Chargetrigger>().m_canTrigger == true && m_chargebar.transform.localScale.x <= 1)
+            {
+                if (Input.GetKeyDown(KeyCode.G))
+                {
+                    m_chargebar.transform.localScale += new Vector3(0.1f, 0, 0);
+                }
+            }
+            if (m_chargebar.transform.localScale.x >= 1)
+            {
+                if(Input.GetKeyDown(KeyCode.E))
+                {
+                    m_EndPhase.GrabPart();
+                    Debug.Log("yes");
+                }
+                
+            }
+        }
+    }
+}
+
+
---
+++ Chargetrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CoreTeamTwan;
+
+public class Chargetrigger : Trigger
+{
+    public bool m_canTrigger;
+
+    public override void TriggerEnter()
+    {
+        m_canTrigger = true;
+        Debug.Log("boop");
+    }
+
+    public override void TriggerLeave()
+    {
+        m_canTrigger = false;
+    }
+}
